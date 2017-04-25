84eaff64ff6d981fc1d726c0d18e53de236cbc95
Rajesh Pillai <pillai.rajesh@gmail.com>
2017-04-25 18:41:15 +0530

    Moved Landing.js to components folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import Landing from './Landing';
+import Landing from './components/Landing';
 
 ReactDOM.render(
   <Landing />,
